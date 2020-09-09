69046ee6bbfee98bf8643c7935362c6dd9b22758
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-09-09 22:12:50 +0300

    Add web components to browse experience.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { configure } from 'mobx';
 import routeStore from './stores/routeStore';
 import './components/app-root';
+import './index.css';
 
 configure({
   enforceActions: 'observed',
