b825466a6c028fccba5d1b3ef9b4fb157fbbac85
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-09-21 22:52:35 +0300

    Add an experimental counter impl.

--- src/components/app-counter.js
+++ src/components/app-counter.js
@@ -1,17 +1,81 @@
-import { html } from 'lit-element';
+import { html, render } from 'lit-html';
 import { classMap } from 'lit-html/directives/class-map';
+import { autorun } from 'mobx';
 import { dashCircle, plusCircle, xCircle } from './icons';
-import { MobxLitElement } from '../utils';
 import { counterStore } from '../stores';
+import { addReaction, clearReactions } from '../utils';
 import './app-counter-modal';
 import './app-counter.css';
 
-export class AppCounter extends MobxLitElement {
+// This is an experimental custom element / web component
+// implemented without LitElement.
+
+class AppCounter extends HTMLElement {
+  constructor() {
+    super();
+    addReaction(
+      this,
+      autorun(() => {
+        const value = counterStore.value;
+        render(
+          html`
+            <section
+              class="d-flex align-items-center justify-content-center p-4"
+            >
+              <div class="card app-counter-card">
+                <div class="card-body">
+                  <h1
+                    class=${classMap({
+                      'card-title': true,
+                      'display-1': true,
+                      'text-danger': value < 0,
+                      'text-success': 0 < value,
+                    })}
+                  >
+                    ${value}
+                  </h1>
+                  <p class="card-text">Counter</p>
+                  <a
+                    href
+                    class="btn btn-link text-danger"
+                    @click="${this.handleDecrement.bind(this)}"
+                  >
+                    ${dashCircle}
+                  </a>
+                  <a
+                    href
+                    class="btn btn-link text-success"
+                    @click="${this.handleIncrement.bind(this)}"
+                  >
+                    ${plusCircle}
+                  </a>
+                  <a
+                    href
+                    class="btn btn-link"
+                    @click=${this.handleReset.bind(this)}
+                  >
+                    ${xCircle}
+                  </a>
+                </div>
+              </div>
+            </section>
+            <app-counter-modal
+              @modal-ok=${this.handleResetOk.bind(this)}
+              @modal-cancel=${this.handleResetCancel.bind(this)}
+            ></app-counter-modal>
+          `,
+          this,
+        );
+      }),
+    );
+  }
+
   disconnectedCallback() {
+    clearReactions(this);
+
     this.modal?.hide();
     this.modal?.dispose();
     this.modal = undefined;
-    super.disconnectedCallback();
   }
 
   handleIncrement(event) {
@@ -40,55 +104,6 @@ export class AppCounter extends MobxLitElement {
 
   handleResetCancel(_event) {
     this.modal?.hide();
-  }
-
-  render() {
-    const value = counterStore.value;
-
-    return html`
-      <section class="d-flex align-items-center justify-content-center p-4">
-        <div class="card app-counter-card">
-          <div class="card-body">
-            <h1
-              class=${classMap({
-                'card-title': true,
-                'display-1': true,
-                'text-danger': value < 0,
-                'text-success': 0 < value,
-              })}
-            >
-              ${value}
-            </h1>
-            <p class="card-text">Counter</p>
-            <a
-              href
-              class="btn btn-link text-danger"
-              @click="${this.handleDecrement}"
-            >
-              ${dashCircle}
-            </a>
-            <a
-              href
-              class="btn btn-link text-success"
-              @click="${this.handleIncrement}"
-            >
-              ${plusCircle}
-            </a>
-            <a href class="btn btn-link" @click="${this.handleReset}">
-              ${xCircle}
-            </a>
-          </div>
-        </div>
-      </section>
-      <app-counter-modal
-        @modal-ok=${this.handleResetOk}
-        @modal-cancel=${this.handleResetCancel}
-      ></app-counter-modal>
-    `;
-  }
-
-  createRenderRoot() {
-    return this;
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -41,3 +41,19 @@ export function createElement({ tagName, props }) {
   const ctor = customElements.get(tagName);
   return ctor ? Object.assign(document.createElement(tagName), props) : null;
 }
+
+export function addReaction(target, reactionDisposer) {
+  (target._r ??= []).push(reactionDisposer);
+}
+
+export function clearReactions(target) {
+  target._r && target._r.forEach((r) => r());
+}
+
+export function addInterval(target, intervalHandle) {
+  (target._i ??= []).push(intervalHandle);
+}
+
+export function clearIntervals(target) {
+  target._i && target._i.forEach((h) => clearInterval(h));
+}
