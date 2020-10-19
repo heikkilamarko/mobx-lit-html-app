ae62ddab0ea6765e5e95667df4dac351bc4eece9
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-10-19 16:37:29 +0300

    Remove content overflow class.

--- src/components/app-root.js
+++ src/components/app-root.js
@@ -8,7 +8,7 @@ class AppRoot extends HTMLElement {
     render(
       html`
         <app-navbar></app-navbar>
-        <app-content class="overflow-auto"></app-content>
+        <app-content></app-content>
       `,
       this,
     );
