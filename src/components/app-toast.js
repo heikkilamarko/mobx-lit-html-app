e5866cdf6305587e02726a4531061fefa4871667
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-10-24 13:12:23 +0300

    Fix toast styles.

--- src/components/app-toast.js
+++ src/components/app-toast.js
@@ -9,20 +9,23 @@ class AppToast extends HTMLElement {
 
       return html`
         <div
-          class="toast position-fixed d-flex align-items-center text-white bg-${toast.type} border-0"
+          class="toast position-fixed text-white bg-${toast.type} border-0"
           style="bottom:1rem;right:1rem"
           role="alert"
           aria-live="assertive"
           aria-atomic="true"
           data-delay=${toast.delay}
         >
-          <div class="toast-body">${toast.message}</div>
-          <button
-            type="button"
-            class="btn-close btn-close-white ml-auto mr-2"
-            data-dismiss="toast"
-            aria-label="Close"
-          ></button>
+          <div class="d-flex align-items-center">
+            <div class="toast-body">${toast.message}</div>
+            <button
+              type="button"
+              class="btn-close btn-close-white ml-auto mr-2"
+              style="flex:1 0 auto"
+              data-dismiss="toast"
+              aria-label="Close"
+            ></button>
+          </div>
         </div>
       `;
     });
