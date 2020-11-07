63cc5156f05cc37f247eb5e634736fdb77fa42da
Marko Heikkilä <heikkila_marko@hotmail.com>
2020-11-07 15:31:58 +0200

    Remove datagrid.scss

--- src/components/app-datagrid.js
+++ src/components/app-datagrid.js
@@ -2,68 +2,6 @@ import { Grid } from 'ag-grid-community';
 import { stores } from '../stores';
 import { addWatchReaction, clearReactions } from '../utils';
 import NameCellRenderer from './datagrid/NameCellRenderer';
-import './app-datagrid.css';
-
-function createGridDiv() {
-  const div = document.createElement('div');
-  div.classList.add('ag-theme-alpine', 'app-datagrid');
-  return div;
-}
-
-function createGrid(gridDiv, rowData) {
-  const defaultColDef = {
-    headerValueGetter,
-    lockVisible: true,
-    sortable: true,
-    filter: true,
-    resizable: true,
-    floatingFilter: true,
-    filterParams: { newRowsAction: 'keep' },
-  };
-
-  const columnDefs = [
-    { field: 'name', sort: 'asc', cellRenderer: NameCellRenderer },
-    { field: 'language' },
-    { field: 'country' },
-    { field: 'job_title' },
-    { field: 'notes' },
-  ];
-
-  const gridOptions = {
-    defaultColDef,
-    columnDefs,
-    rowData,
-    pagination: true,
-    paginationPageSize: 10,
-    domLayout: 'autoHeight',
-    localeTextFunc: localeTextFunc.bind(this),
-    onFirstDataRendered: onFirstDataRendered.bind(this),
-  };
-
-  return new Grid(gridDiv, gridOptions);
-}
-
-function headerValueGetter({ colDef }) {
-  return stores.i18nStore.t(`datagrid.${colDef.field}`);
-}
-
-function localeTextFunc(key, defaultValue) {
-  return stores.i18nStore.t(`ag_grid.${key}`, null, defaultValue);
-}
-
-function onFirstDataRendered({ api, columnApi }) {
-  let state = sessionStorage.getItem('app-gridstate');
-
-  if (state) {
-    state = JSON.parse(state);
-    columnApi.setColumnState(state.columnState);
-    api.setFilterModel(state.filterModel);
-    api.paginationSetPageSize(state.pageSize);
-    api.paginationGoToPage(state.currentPage);
-  } else {
-    columnApi.autoSizeAllColumns();
-  }
-}
 
 class AppDatagrid extends HTMLElement {
   connectedCallback() {
@@ -119,6 +57,67 @@ class AppDatagrid extends HTMLElement {
 
   get gridColumnApi() {
     return this.grid.gridOptions.columnApi;
+  }
+}
+
+function createGridDiv() {
+  const div = document.createElement('div');
+  div.classList.add('ag-theme-alpine');
+  return div;
+}
+
+function createGrid(gridDiv, rowData) {
+  const defaultColDef = {
+    headerValueGetter,
+    lockVisible: true,
+    sortable: true,
+    filter: true,
+    resizable: true,
+    floatingFilter: true,
+    filterParams: { newRowsAction: 'keep' },
+  };
+
+  const columnDefs = [
+    { field: 'name', sort: 'asc', cellRenderer: NameCellRenderer },
+    { field: 'language' },
+    { field: 'country' },
+    { field: 'job_title' },
+    { field: 'notes' },
+  ];
+
+  const gridOptions = {
+    defaultColDef,
+    columnDefs,
+    rowData,
+    pagination: true,
+    paginationPageSize: 10,
+    domLayout: 'autoHeight',
+    localeTextFunc: localeTextFunc.bind(this),
+    onFirstDataRendered: onFirstDataRendered.bind(this),
+  };
+
+  return new Grid(gridDiv, gridOptions);
+}
+
+function headerValueGetter({ colDef }) {
+  return stores.i18nStore.t(`datagrid.${colDef.field}`);
+}
+
+function localeTextFunc(key, defaultValue) {
+  return stores.i18nStore.t(`ag_grid.${key}`, null, defaultValue);
+}
+
+function onFirstDataRendered({ api, columnApi }) {
+  let state = sessionStorage.getItem('app-gridstate');
+
+  if (state) {
+    state = JSON.parse(state);
+    columnApi.setColumnState(state.columnState);
+    api.setFilterModel(state.filterModel);
+    api.paginationSetPageSize(state.pageSize);
+    api.paginationGoToPage(state.currentPage);
+  } else {
+    columnApi.autoSizeAllColumns();
   }
 }
 
