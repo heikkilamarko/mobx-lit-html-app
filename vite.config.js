c783f08b96e3eefd2c34853327ac45bfd8244920
Marko Heikkilä <heikkila_marko@hotmail.com>
2021-07-22 12:30:30 +0300

    Minor updates.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 2048,
+  },
   css: {
     preprocessorOptions: {
       scss: {
