712d4f96b973ff6fe856028b462e63ac34da1a0e
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-05-24 20:26:41 +0300

    Cleanup.

--- src/shared/routes.js
+++ src/shared/routes.js
@@ -1,69 +1,65 @@
 import { html } from 'lit';
 
-export const routes = addActions([
-	{
+export const routes = [
+	withAction({
 		name: 'browse',
 		path: '/browse',
 		template: html`<app-browse></app-browse>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'browse.detail',
 		path: '/browse/:id',
 		template: html`<app-detail></app-detail>`,
 		navbar: false
-	},
-	{
+	}),
+	withAction({
 		name: 'counter',
 		path: '/counter',
 		template: html`<app-counter></app-counter>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'jokes',
 		path: '/jokes',
 		template: html`<app-jokes></app-jokes>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'datagrid',
 		path: '/datagrid',
 		template: html`<app-datagrid></app-datagrid>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'charts',
 		path: '/charts',
 		template: html`<app-charts></app-charts>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'form',
 		path: '/form',
 		template: html`<app-form></app-form>`,
 		navbar: true
-	},
-	{
+	}),
+	withAction({
 		name: 'widgets',
 		path: '/widgets',
 		template: html`<app-widgets></app-widgets>`,
 		navbar: true
+	}),
+	{
+		path: '*all',
+		action: () => ({ redirect: '/browse' })
 	}
-]);
+];
 
-routes.push({
-	path: '*all',
-	action: () => ({ redirect: '/browse' })
-});
+function withAction(route) {
+	route.action = (ctx) => ({
+		...route,
+		params: { ...ctx.params, ...ctx.queryParams }
+	});
 
-function addActions(routes) {
-	routes.forEach(
-		(route) =>
-			(route.action = (ctx) => ({
-				...route,
-				params: { ...ctx.params, ...ctx.queryParams }
-			}))
-	);
-
-	return routes;
+	return route;
 }
